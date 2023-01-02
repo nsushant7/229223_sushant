5e5c35b6c6f2785abe073864539677aa562227d5
dac <dac@dypm.org>
2023-01-02 15:18:21 +0530

    myprofile

--- server.js
+++ server.js
@@ -1,13 +1,47 @@
-var express=require ("express");
-var app=express();
-// var bodyparse=require("body-parser");
-// const { urlencoded } = require("body-parser");
+var express=require('express');
+var session =require('express-session');
+var parseurl=require('parseurl');
+var path=require('path');
+var bodyParser=require('body-parser');
+
+const app=express();
 
 
-app.get("/",(req,res)=>{
-    res.send("IN HOME PAGE")
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+var sessionOptions={
+   secret:"secret",
+   resave:true,
+   saveUninitialized:false
+};
+app.use(session(sessionOptions));
+
+
+app.use(function(req, res,next){
+
+   if (!req.session.views) {
+      req.session.views = {};
+      req.session.views.profile=[];
+   }
+
+   var pathname=parseurl(req).pathname;
+
+   req.session.views[pathname] = (req.session.views[pathname] || 0) + 1;
+   next();
 })
-app.get("/profile",(req,res)=>{
-    res.send("in my profile")
-})
+
+
+
+var staticFolder=express.static(path.join(__dirname,"public"));
+app.use(staticFolder);
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+// app.get("/", (req, res)=>{
+//    res.sendFile(path.join(__dirname + '/profile.html'));
+// });
 app.listen(4000);
+console.log("Profile port 4000");
